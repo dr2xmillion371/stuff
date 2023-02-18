1af8f24f8191d88f373f17d53e49151e2ba27ca5
dr2xmillion371 <119767559+dr2xmillion371@users.noreply.github.com>
2023-02-18 12:24:12 -0600

    numlist-csharp

---
+++ list-from-1-to-1000.cs
@@ -0,0 +1,17 @@
+// start
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      for (int i = 0; i < 1001; i++) 
+      {
+        Console.WriteLine(i);
+      }    
+    }
+  }
+}
+// end
