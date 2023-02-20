127faa5f85c1cd5912a136f0413b8a366e404f2d
dr2xmillion371 <119767559+dr2xmillion371@users.noreply.github.com>
2023-02-20 10:35:24 -0600

    bl python source code item 1

---
+++ branch_log-source-code[2].py
@@ -0,0 +1,17 @@
+######################
+## START
+######################
+
+
+print("Welcome to branch log!, it's a very basic program!")
+print()
+print()
+print("Here is our number displayer program!")
+users_text=input("Please enter a number to display: ")
+eval(users_text)
+print(users_text)
+
+
+#####################
+## END
+#####################
