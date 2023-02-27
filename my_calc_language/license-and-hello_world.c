b7fd541abf1941fe6eb5fdeccaf081d54b709528
dr2xmillion371 <119767559+dr2xmillion371@users.noreply.github.com>
2023-02-27 17:40:53 -0600

    licenseandhelloworld

---
+++ my_calc_language/license-and-hello_world.c
@@ -0,0 +1,12 @@
+// this code may be used by anyone
+// you don't have to credit me, but it would be appreciated if you did though.
+// conclusion: free
+
+
+// hello world example
+#include <stdio.h>
+
+int main() {
+  printf("Hello, world!");
+  return 0;
+}
