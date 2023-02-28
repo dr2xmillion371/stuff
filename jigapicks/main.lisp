a14c17b719a245f554f3d21159cf341acd67eba9
dr2xmillion371 <119767559+dr2xmillion371@users.noreply.github.com>
2023-02-27 19:36:01 -0600

    main

---
+++ jigapicks/main.lisp
@@ -0,0 +1,11 @@
+;start
+(write (string="welcome to jigapicks!"))
+(write (string="hope your having a good day!"))
+(write (string="i am having a good day too"))
+(write (string="the purpose of this program is too make you read a little bit"))
+(write (string="the man jumped over the hill!"))
+(write (string="that's impossible!"))
+(write (string="it is impossible"))
+(write (string="enless if the hill was extremely short"))
+(write (string="the end."))
+;end
