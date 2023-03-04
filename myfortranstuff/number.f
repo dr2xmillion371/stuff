b78b3809a36f53a245135a78f6c3411e9574b7a9
dr2xmillion371 <119767559+dr2xmillion371@users.noreply.github.com>
2023-03-04 12:11:27 -0600

    number

---
+++ myfortranstuff/number.f
@@ -0,0 +1,24 @@
+program number
+implicit none
+
+   ! variables
+   a=0
+   b=100
+   
+   ! printing the variables
+   print *, a
+   print *, b
+   
+   ! more variables section 2
+   j=78
+   g=32
+   t=1314
+   h=1000
+   
+   ! printing the variables
+   print *, j
+   print *, g
+   print *, t
+   print *, h
+
+end program number
